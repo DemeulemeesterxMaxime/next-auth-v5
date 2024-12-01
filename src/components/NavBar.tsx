e340198790596988e06ef1d3fbabf62299414e60
DemeulemeesterxMaxime <maxime.demeulemeester@student.junia.com>
2024-12-01 23:52:33 +0100

    mise a jour nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React from "react";
 import useTheme from "@/app/hooks/useTheme";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession, signOut } from "next-auth/react";
+import Image from "next/image";
+import UserButton from "./UserButton";
 import {
   Navbar,
   NavbarBrand,
@@ -19,10 +21,10 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import { ShoppingOutlined } from "@ant-design/icons";
-import { AcmeLogo } from "../nextui/adds/AcmeLogo.jsx";
 import { usePathname } from "next/navigation"; // Import du hook pour obtenir le chemin actuel
 
 export default function Header() {
+  const session = useSession();
   const user = session.data?.user;
   const { theme, toggleTheme } = useTheme();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -44,25 +46,36 @@ export default function Header() {
         />
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">3D Printing Online</p>
-        </NavbarBrand>
-      </NavbarContent>
+      <NavbarBrand>
+        <Image
+          src="/3D.svg"
+          width={50}
+          height={50}
+          alt="Platform Logo"
+          priority
+        />
+        <p className="font-bold text-inherit">3D Printing Online</p>
+      </NavbarBrand>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        {/* <NavbarBrand>
+          <Image
+            src="/3D.svg"
+            width={50}
+            height={50}
+            alt="Platform Logo"
+            priority
+          />
           <p className="font-bold text-inherit">3D Printing Online</p>
-        </NavbarBrand>
+        </NavbarBrand> */}
         {menuItems.map((item) => (
           <NavbarItem key={item.name} isActive={pathname === item.href}>
             <Link href={item.href} passHref>
               <span
                 style={{
                   color: pathname === item.href ? "foreground" : "inherit",
-                }}>
+                }}
+              >
                 {item.name}
               </span>
             </Link>
@@ -71,6 +84,24 @@ export default function Header() {
       </NavbarContent>
 
       <NavbarContent justify="end">
+        <NavbarItem>
+          {/* <button
+            onClick={toggleTheme}
+            className="flex items-center justify-center rounded-md bg-gray-200 p-2 
+                       text-gray-800 transition-colors hover:bg-gray-300 dark:bg-gray-700 
+                       dark:text-gray-200 dark:hover:bg-gray-600"
+          >
+            {theme === "dark" ? "☀️" : "🌙"}
+          </button> */}
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Toggle Theme"
+            onClick={toggleTheme}
+          >
+            {theme === "dark" ? "☀️" : "🌙"}
+          </Button>
+        </NavbarItem>
         <NavbarItem>
           <Link href="/panier">
             <ShoppingOutlined />
@@ -86,13 +117,13 @@ export default function Header() {
                 color="secondary"
                 name={user?.name || "User"}
                 size="sm"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                src={user?.image}
               />
             </DropdownTrigger>
             <DropdownMenu aria-label="Profile Actions" variant="flat">
               <DropdownItem key="profile" className="h-14 gap-2">
-                <p className="font-semibold">Signed in as</p>
-                <p className="font-semibold">zoey@example.com</p>
+                <p className="font-semibold">Signed in as {user?.name}</p>
+                <p className="font-semibold">{user?.email}</p>
               </DropdownItem>
               <DropdownItem key="settings">My Settings</DropdownItem>
               <DropdownItem key="team_settings">Team Settings</DropdownItem>
@@ -102,14 +133,19 @@ export default function Header() {
               <DropdownItem key="help_and_feedback">
                 Help & Feedback
               </DropdownItem>
-              <DropdownItem key="logout" color="danger" onClick={handleLogout}>
+              <DropdownItem
+                key="logout"
+                color="danger"
+                onClick={() => signOut({ callbackUrl: "/" })}
+              >
                 Log Out
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
         ) : (
           <NavbarItem>
-            <Modal_log />
+            {/* Bouton utilisateur ou connexion */}
+            {session.status !== "loading" && <SignInButton />}
           </NavbarItem>
         )}
       </NavbarContent>
@@ -122,7 +158,8 @@ export default function Header() {
                 style={{
                   color: pathname === item.href ? "foreground" : "inherit",
                   width: "100%",
-                }}>
+                }}
+              >
                 {item.name}
               </span>
             </Link>
@@ -130,5 +167,13 @@ export default function Header() {
         ))}
       </NavbarMenu>
     </Navbar>
+  );
+}
+
+function SignInButton() {
+  return (
+    <Button onClick={() => signIn()} className="px-4 py-2">
+      Sign in
+    </Button>
   );
 }
